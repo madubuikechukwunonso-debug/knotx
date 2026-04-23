ed224fc0bac73db5ee2721017ec0d8dfc58adaf6
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-23 07:05:44 -0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
-import * as schema from '../../../db/schema';
-import * as relations from '../../../db/relations';
+import * as schema from '../../db/schema';
+import * as relations from '../../db/relations';
 import { env } from './env';
 
 const fullSchema = { ...schema, ...relations };
