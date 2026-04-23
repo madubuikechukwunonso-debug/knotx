9c1c898484122d6edc96573a94d27718b6357402
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-23 08:13:16 -0700

    Update orders.types.ts

--- src/modules/orders/orders.types.ts
+++ src/modules/orders/orders.types.ts
@@ -1,7 +1,19 @@
-export type OrderInput = {
+export type OrderSessionUser = {
+  userId: number;
+  userType: "local" | "oauth";
+  email?: string;
+  role?: "user" | "worker" | "admin" | "super_admin";
+  name?: string;
+};
+
+export type CreateOrderInput = {
+  items: {
+    productId: number;
+    quantity: number;
+  }[];
   customerName: string;
   customerEmail: string;
+  customerPhone?: string;
   userId?: number;
-  userType?: 'local' | 'oauth';
-  items: { productId: number; quantity: number }[];
+  userType?: "local" | "oauth";
 };
