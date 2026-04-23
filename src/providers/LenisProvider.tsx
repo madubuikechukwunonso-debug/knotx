3c0d03744c76fa71c6de37f1f93e1acef89dfbe6
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-23 09:31:16 -0700

    Update LenisProvider.tsx

--- src/providers/LenisProvider.tsx
+++ src/providers/LenisProvider.tsx
@@ -9,7 +9,8 @@ export default function LenisProvider({ children }: { children: ReactNode }) {
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false,
+      touchMultiplier: 2,     // ← Fixed: replaced smoothTouch
+      infinite: false,
     });
 
     function raf(time: number) {
