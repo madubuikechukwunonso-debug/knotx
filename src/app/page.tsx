1bd6f3fb5956a61826582d840277e06cca2fb28d
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-23 08:25:02 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Navigation from '@/components/Navigation';
-import HeroSection from '@/components/HeroSection';
-import ManifestoSection from '@/components/ManifestoSection';
-import HomeGallerySection from '@/components/HomeGallerySection';
-import ServicesSection from '@/components/ServicesSection';
-import ProductsSection from '@/components/ProductsSection';
-import NewsletterSection from '@/components/NewsletterSection';
-import Footer from '@/components/Footer';
+import Navigation from "@/components/Navigation";
+import HeroSection from "@/components/HeroSection";
+import ManifestoSection from "@/components/ManifestoSection";
+import HomeGallerySection from "@/components/HomeGallerySection";
+import ServicesSection from "@/components/ServicesSection";
+import ProductsSection from "@/components/ProductsSection";
+import NewsletterSection from "@/components/NewsletterSection";
+import Footer from "@/components/Footer";
+import { listProducts } from "@/modules/products/products.service";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const products = await listProducts();
+
   return (
     <>
       <Navigation />
@@ -15,7 +18,7 @@ export default function HomePage() {
       <ManifestoSection />
       <HomeGallerySection />
       <ServicesSection />
-      <ProductsSection />
+      <ProductsSection products={products} />
       <NewsletterSection />
       <Footer />
     </>
