a8cd25316af4db4c523b8b40229471c3c92b778d
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-26 23:45:08 -0700

    Update AdminDashboardPage.tsx

--- src/src-pages/AdminDashboardPage.tsx
+++ src/src-pages/AdminDashboardPage.tsx
@@ -1,7 +1,7 @@
-import { requireAdmin } from '@/lib/permissions';
+// src/src-pages/AdminDashboardPage.tsx
 import AdminSubPage from './AdminSubPage';
 
 export default async function AdminDashboardPage() {
-  await requireAdmin();
+  // requireAdmin() is already handled by layout.tsx
   return <AdminSubPage tab="overview" />;
 }
