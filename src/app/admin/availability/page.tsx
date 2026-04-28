12222011e9e6003e7475f0ca1cf24d9f7dc0aba1
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-28 00:58:19 -0700

    Create page.tsx

---
+++ src/app/admin/availability/page.tsx
@@ -0,0 +1,6 @@
+// src/app/admin/availability/page.tsx
+import AdminAvailabilitySection from '@/sections/admin/AdminAvailabilitySection';
+
+export default function AvailabilityPage() {
+  return <AdminAvailabilitySection />;
+}
