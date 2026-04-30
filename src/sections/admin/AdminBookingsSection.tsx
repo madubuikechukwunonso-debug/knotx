e70ff36d66414f4d5f585198dde486947b92e517
madubuikechukwunonso-debug <madubuikechukwunonso@gmail.com>
2026-04-30 12:33:39 -0700

    Update AdminBookingsSection.tsx

--- src/sections/admin/AdminBookingsSection.tsx
+++ src/sections/admin/AdminBookingsSection.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '@/lib/prisma';
 import { revalidatePath } from 'next/cache';
 import AdminBookingTable from './AdminBookingTable';
 import { Plus } from 'lucide-react';
